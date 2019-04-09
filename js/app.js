3f54ecf962b19e65e360c208d2d98afb637b8bb8
DarbNho_78 <druizbermud@uniminuto.edu.co>
2019-04-09 17:53:23 -0500

    Funcionalidad de los botones

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 var Calculadora = (function(document, undefined){
-  var error_op = "Operación no soportada.";
+  var error_op = "La operación solicitada no existe.";
   var newVal = false,
       maxLen = 9,
       nOp1 = 0,
       nOp2 = 0,
-      result = 0,     
+      result = 0,
+      hasPoint = false;
+      screenVal = "0",
       idBOp = "";
 
   var btnsId  = {
@@ -18,3 +20,51 @@ var Calculadora = (function(document, undefined){
     btn_igual: "igual",
     btn_mas  : "mas"
   };
+
+   var domBtns;
+
+  function initVars(){
+    newVal = false;
+    maxLen = 9;
+    nOp1 = 0;
+    nOp2 = 0;
+    result = 0;
+    hasPoint = false;
+    screenVal = "0",
+    idBOp = "";
+  }
+
+  var getBtns = function(){
+    domBtns = document.getElementsByClassName("tecla");
+  }
+
+  var subscribeEvents = function(){
+    for(var i = 0, len = domBtns.length; i < len; i++) {
+        domBtns[i].onclick = events.eBtnClick;
+    }
+  }
+
+var events = {
+    eBtnClick: function(e){
+      switch (this.id) {
+        case btnsId.btn_onc  :
+          initVars();
+          writeDisplay(screenVal);
+          break;
+        case btnsId.btn_sign :
+          setSign();
+          writeDisplay(screenVal);
+          break;
+        case btnsId.btn_raiz :
+          break;
+        case btnsId.btn_punto:
+          addPoint();
+          break;
+        case btnsId.btn_igual:
+          solveOp();
+          break;      
+        default:
+          addNumber(this.id);
+      }
+    }
+  }
